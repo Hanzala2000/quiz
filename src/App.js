c0bf32d0fc46235a23952369231ee79f816cc890
Hanzala2000 <hanzala.irfan@gmail.com>
2022-02-06 22:58:19 +0500

    quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  let [currentQues,setCurrentQues] = useState(0)
+  let [score,setScore] = useState(1)
+  
+  let questionArr = [
+    {
+      question: "what is 2+2",
+      answer:[
+        {answer:2,correct:false},
+        {answer:6,correct:false},
+        {answer:4,correct:true},
+        {answer:0,correct:false},
+      ]
+    },
+    {
+      question: "what is 6+2",
+      answer:[
+        {answer:2,correct:false},
+        {answer:8,correct:true},
+        {answer:4,correct:false},
+        {answer:0,correct:false},
+      ]
+    },
+    {
+      question: "what is 4+2",
+      answer:[
+        {answer:8,correct:false},
+        {answer:6,correct:true},
+        {answer:4,correct:false},
+        {answer:0,correct:false},
+      ]
+    },
+  ]
+  let changeQues = (correct)=>{
+    if (correct == true) {
+      alert("correct")
+      setScore(score+1)
+    }else if(correct == false){
+      alert("Wrong")
+    }
+    let nextQues = currentQues+1
+    if (nextQues<questionArr.length) {
+      setCurrentQues(nextQues)
+    }else{
+      alert("end")
+      alert(score)
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='main1'>
+      <h1>Question {currentQues+1}/{questionArr.length}:{questionArr[currentQues].question}</h1>
+      </div>
+      <div className='main2'>
+        {questionArr[currentQues].answer.map((answer,ind)=>{
+        return  <button onClick={()=>changeQues(answer.correct)} key={ind}>{answer.answer}</button>
+        })}
+      </div>
     </div>
   );
 }
